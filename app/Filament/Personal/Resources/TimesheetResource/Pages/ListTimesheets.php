764372bfeedeb7de176f9be367c78ce5da9c8d0a
Miguel Angel <33709574+miguelapol@users.noreply.github.com>
2025-04-09 19:16:13 -0600

    botones acciones

--- app/Filament/Personal/Resources/TimesheetResource/Pages/ListTimesheets.php
+++ app/Filament/Personal/Resources/TimesheetResource/Pages/ListTimesheets.php
@@ -4,6 +4,8 @@ namespace App\Filament\Personal\Resources\TimesheetResource\Pages;
 
 use App\Filament\Personal\Resources\TimesheetResource;
 use Filament\Actions;
+//CARBON
+use Carbon\carbon;
 use Filament\Resources\Pages\ListRecords;
 use Filament\Tables\Actions\CreateAction;
 use Filament\Notifications\Notification;
@@ -15,6 +17,13 @@ use Illuminate\Support\Facades\Auth;
 use Filament\Forms\Components\Hidden;
 use Filament\Actions\Action;
 
+//model Timesheet
+use App\Models\Timesheet;
+//Calendar
+use App\Models\Calendar;
+
+
+
 class ListTimesheets extends ListRecords
 
 {
@@ -25,10 +34,133 @@ class ListTimesheets extends ListRecords
         return [
             Action::make('in Work')
                 ->label('Entrar a trabajar')->color('success')
+                ->action(function () {
+                    $user=Auth::user();
+                    //verificar que day_out este null entonces salta notificacion
+                    $openWorkSession=Timesheet::where('user_id', $user->id)
+                        ->where('type', 'work')
+                        ->where('day_out', null)
+                        ->latest()
+                        ->first();
+                        if ($openWorkSession) {
+                            Notification::make()
+                                ->title('Ya tienes una sesión de trabajo activa')
+                                ->danger()
+                                ->send();
+                            return;
+                        }
+                        //obtenemos el ultimo registro agregado de calendar
+                        $calendarUltimate=Calendar::latest()->first();
+
+                        Timesheet::create([
+                            'user_id' => $user->id,
+                            'calendar_id' =>$calendarUltimate->id,
+                            'type' => 'work',
+                            'day_in' => Carbon::now(),
+                            'day_out' => null,
+                        ]);
+                          Notification::make()
+                        ->title('¡Sesión de trabajo iniciada!')
+                        ->success()
+                        ->send();
+                }),
+            Action::make('in Work pause')
+                ->label('Pause work')->color('success')
+                ->action(function (){
+                    //lo que harmeos es aqui sera algo parecido pero no
+                    $user=Auth::user();
+                    //verificar que day_out este null entonces salta notificacion
+                    $openWorkSession=Timesheet::where('user_id', $user->id)
+                        ->where('type', 'work')
+                        ->whereNull('day_out')
+                        ->latest()
+                        ->first();
+                        if (!$openWorkSession) {
+                            Notification::make()
+                                ->title('Trabajo no iniciado') // Notify that no work session has been started
+                                ->danger()
+                                ->send();
+                            return;
+                        }
+                        $Timesheet=Timesheet::where('user_id', $user->id)
+                        ->where('type', 'work')
+                        ->WhereNull('day_out')
+                        ->latest()
+                        ->first();
+                        //editamps el timessheet del ese ultimo registro
+                        $Timesheet->update([
+                            'day_out' => Carbon::now(),
+                        ]);
+                    Notification::make()
+                        ->title('Sesion de trabajo terminado')
+                        ->success()
+                        ->send();
+                }
+                )
                 ->requiresConfirmation(),
-            Action::make('in Pause')
+            Action::make('startPause')
                 ->label('Comenza a pausar')->color('info')
-                ->requiresConfirmation(),
+                ->action(function(){
+                    $user=Auth::user();
+                    //verificar que day_out este null entonces salta notificacion
+                    $openWorkSession=Timesheet::where('user_id', $user->id)
+                        ->where('type', 'pause')
+                        ->whereNull('day_out')
+                        ->latest()
+                        ->first();
+                        if ($openWorkSession) {
+                            Notification::make()
+                                ->title('Ya tienes una sesión de pausa activa')
+                                ->danger()
+                                ->send();
+                            return;
+                        }
+                        //obtenemos el ultimo registro agregado de calendar
+                        $calendarUltimate=Calendar::latest()->first();
+
+                        Timesheet::create([
+                            'user_id' => $user->id,
+                            'calendar_id' =>$calendarUltimate->id,
+                            'type' => 'pause',
+                            'day_in' => Carbon::now(),
+                            'day_out' => null,
+                        ]);
+                          Notification::make()
+                        ->title('¡Sesión de pausa iniciada!')
+                        ->success()
+                        ->send();
+                }),
+            Action::make('stopPause')
+                ->label('Detener pausar')->color('info')
+                ->action(function(){
+                    $user=Auth::user();
+                    //verificar que day_out este null entonces salta notificacion
+                    $openWorkSession=Timesheet::where('user_id', $user->id)
+                        ->where('type', 'pause')
+                        ->whereNull('day_out')
+                        ->latest()
+                        ->first();
+                        if (!$openWorkSession) {
+                            Notification::make()
+                                ->title('Pausa no iniciada') // Notify that no work session has been started
+                                ->danger()
+                                ->send();
+                            return;
+                        }
+                        $Timesheet=Timesheet::where('user_id', $user->id)
+                        ->where('type', 'pause')
+                        ->WhereNull('day_out')
+                        ->latest()
+                        ->first();
+                        //editamps el timessheet del ese ultimo registro
+                        $Timesheet->update([
+                            'day_out' => Carbon::now(),
+                        ]);
+                    Notification::make()
+                        ->title('Sesion de pausa terminado')
+                        ->success()
+                        ->send();
+                }),
             Actions\CreateAction::make(),
         ];
     }
