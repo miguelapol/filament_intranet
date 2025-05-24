eb39bc11b9c414d183d533dc212cd5528407f2ff
Miguel Angel <33709574+miguelapol@users.noreply.github.com>
2025-05-24 07:55:46 -0600

    refactor(TimesheetResource): alinear los métodos de la acción masiva de exportación y mejorar la funcionalidad de importación
    - Alinear los métodos de la acción masiva de exportación para mejorar la legibilidad
    - Añadir acción de importación de Excel con validación de calendario y notificaciones
    - Actualizar ListTimesheetsImport para gestionar la recopilación y validar la existencia de calendario

--- app/Imports/ListTimesheetsImport.php
+++ app/Imports/ListTimesheetsImport.php
@@ -2,20 +2,58 @@
 
 namespace App\Imports;
 
+//calendar
+use App\Models\Calendar;
+//carbon para fecha
+use Carbon\Carbon;
 use App\Models\Timesheet;
-use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
+//auth
+use Illuminate\Support\Facades\Auth;
 
-class ListTimesheetsImport implements ToModel
+//collection
+use Illuminate\Support\Collection;
+use Maatwebsite\Excel\Concerns\ToCollection;
+
+use Filament\Notifications\Notification;
+
+
+class ListTimesheetsImport implements ToCollection, WithHeadingRow
 {
     /**
     * @param array $row
     *
     * @return \Illuminate\Database\Eloquent\Model|null
     */
-    public function model(array $row)
+    public function collection(Collection $rows)
     {
-        return new Timesheet([
-            //
+
+    foreach ($rows as $row) {
+        $calendar_id = Calendar::where('name', $row['calendario'])->first();
+
+        if ($calendar_id === null) {
+            Notification::make()
+                ->title('Error de importación')
+                ->warning()
+                ->body('No existe el calendario: ' . $row['calendario'])
+                ->send();
+            return;
+        }
+         Timesheet::create([
+            'calendar_id' => $calendar_id->id,
+            'user_id' => Auth::id(),
+            'type' => $row['tipo'],
+            'day_in' => $row['hora_de_entrada'],
+            'day_out' => $row['hora_de_salida'],
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
         ]);
+
+    }
+        Notification::make()
+            ->title('Importación exitosa')
+            ->success()
+            ->body('Se han importado correctamente los registros de asistencia.')
+            ->send();
     }
 }
